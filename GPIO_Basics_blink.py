4dc84f78d7ab167d559da58f6b0f31139aa2abc2
mazzo23 <macrobat7@gmail.com>
2019-03-02 20:17:44 +0100

    added try,except,finally for clean exit

--- GPIO_Basics_blink.py
+++ GPIO_Basics_blink.py
@@ -4,8 +4,15 @@ import time
 io.setmode(io.BCM)
 io.setup(24,io.OUT)
 
-while True:
-	io.output(24,1)
-	time.sleep(1)
-	io.output(24,0)
-	time.sleep(1)
+try:
+	while True:
+		io.output(24,1)
+		time.sleep(1)
+		io.output(24,0)
+		time.sleep(1)
+
+except KeyboardInterrupt:
+	print "ontrol + C was pressed, exit with io.cleanup()"
+
+finally:
+	io.cleanup()
