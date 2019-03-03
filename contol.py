5ee15adaf3dd6148757971a86ccb61ff437ed017
mazzo23 <macrobat7@gmail.com>
2019-03-03 19:06:33 +0100

    added LED blink,on and off

--- contol.py
+++ contol.py
@@ -11,20 +11,35 @@ io.setup(18,io.IN)
 io.setup(27,io.IN)
 io.setup(22,io.IN)
 io.setup(23,io.IN)
+io.setup(24,io.OUT)
+
+x = True
 
 try:
     while True:
         #vars are easier to read
-        toggle = io.input(4)    #play/pause; if stopped, starts playing
-        prev = io.input(17)     #previous title
-        seek_f = io.input(18)   #seek forward (+5% of whole song)
-        seek_b = io.input(27)   #seek backward (-5% of the song)
-        next = io.input(22)     #next title
-        stop = io.input(23)     #stop playback
+        toggle = io.input(4)    	#play/pause; if stopped, starts playing
+        prev = io.input(17)     	#previous title
+        seek_f = io.input(18)   	#seek forward (+5% of whole song)
+        seek_b = io.input(27)   	#seek backward (-5% of the song)
+        next = io.input(22)     	#next title
+        stop = io.input(23)     	#stop playback
+        LED_on = io.output(24,1)	#green LED on
+        LED_off = io.output(24,0)	#green LED off
+
+	if x == True:
+	    LED_on
+	    time.sleep(0.5)
+	    LED_off
+	    time.sleep(0.5)
+	if x == False:
+	    LED_off
+
 
         if toggle == 1:
             os.system('mpc toggle')
             time.sleep(0.3)
+            x = not x
         if prev == 1:
             os.system('mpc prev')
             time.sleep(0.3)
